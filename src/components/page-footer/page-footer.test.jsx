6df2091c2c3d479516eef8de21d0b06fed775d51
Devyaterikov Petr <p.devyaterkov@ifrbanking.ru>
2020-04-03 18:57:23 +0300

    Adds page-footer component

---
+++ src/components/page-footer/page-footer.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {MemoryRouter as Router} from 'react-router-dom';
+import PageFooter from './page-footer.jsx';
+
+it(`PageFooter component should renders correctly`, () => {
+  const pageFooter = renderer.create(
+      <Router>
+        <PageFooter />
+      </Router>
+  ).toJSON();
+  expect(pageFooter).toMatchSnapshot();
+});
