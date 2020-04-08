2bcc34e006b5b487635e6b4088e47bf0fded37de
Devyaterikov Petr <p.devyaterkov@ifrbanking.ru>
2020-04-08 22:00:03 +0300

    Finishes simplifying router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import {UrlPath} from '../../shared/const.js';
-
-import withAuth from '../../hocs/with-auth/with-auth.js';
-import MainPageLoader from '../main-page-loader/main-page-loader.jsx';
-import OfferPageLoader from '../offer-page-loader/offer-page-loader.jsx';
-import FavoritesPageLoader from '../favorites-page-loader/favorites-page-loader.jsx';
-import ErrorPage from '../error-page/error-page.jsx';
-import SigInPage from '../sign-in-page/sig-in-page.jsx';
+import {UrlPath} from 'shared/const.js';
+import MainPageLoader from 'components/main-page-loader/main-page-loader.jsx';
+import SigInPage from 'components/sign-in-page/sig-in-page.jsx';
+import withAuth from 'hocs/with-auth/with-auth.js';
+import FavoritesPageLoader from 'components/favorites-page-loader/favorites-page-loader.jsx';
+import OfferPageLoader from 'components/offer-page-loader/offer-page-loader.jsx';
+import ErrorPage from 'components/error-page/error-page.jsx';
 
 const PrivateFavoritesPageLoader = withAuth(FavoritesPageLoader);
 
 const App = () => (
   <Switch>
-    <Route exact path={UrlPath.ROOT} render={() => <MainPageLoader />} />
+    <Route exact path={UrlPath.ROOT} component={MainPageLoader} />
     <Route exact path={UrlPath.LOGIN} component={SigInPage} />
     <Route exact path={UrlPath.FAVORITES} component={PrivateFavoritesPageLoader} />
     <Route exact path={`${UrlPath.OFFER}/:id`} component={OfferPageLoader} />
-    <Route render={() => <ErrorPage />} />
+    <Route component={ErrorPage} />
   </Switch>
 );
 
